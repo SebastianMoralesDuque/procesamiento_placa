573e0b0861f243a963c4d553e3b4d2bf2834e5f0
Sebastian <cheviotin200@gmail.com>
2023-05-18 15:37:34 -0500

    se agrega paginacion

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 export function Navigation() {
   return (
     <div className="flex justify-between py-3 items-center">
-      <Link to="/tasks">
+      <Link to="/placas">
         <h1 className="font-bold text-3xl mb-4">Placas</h1>
       </Link>
       <button className="bg-indigo-500 p-3 rounded-lg">
-        <Link to="/tasks-create">Agregar placa</Link>
+        <Link to="/crear-placa">Agregar placa</Link>
       </button>
     </div>
   );
